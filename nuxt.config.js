42e26d265ca5aac531c7c2101080ff0d7340f41b
Victor Tolbert <victor.tolbert@gmail.com>
2021-06-18 15:23:54 -0400

    add tailwind and oruga

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,17 @@
 export default {
-  serverMiddleware: ["~/server"],
-  target: "static",
+  components: true,
+  buildModules: ['@nuxtjs/tailwindcss'],
+  modules: ['@oruga-ui/oruga/nuxt'],
+  oruga: {
+    includeCss: 'vars'
+  },
+  tailwindcss: {
+    jit: true,
+    // add '~tailwind.config` alias
+    exposeConfig: true
+  },
+  serverMiddleware: ['~/server'],
+  target: 'static',
   generate: {
     exclude: [/spa-fallback/],
   },
